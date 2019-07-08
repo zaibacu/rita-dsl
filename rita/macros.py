bf014ae7fae0426309fd2047889668fa043b37eb
Sarunas Navickas <sarunas.navickas@tokenmill.lt>
2019-07-08 13:26:53 +0300

    Macro for NUM

--- rita/macros.py
+++ rita/macros.py
@@ -8,14 +8,14 @@ def ANY(context, op=None):
     return context
 
 
-def MARK(type_, obj):
+def MARK(type_, obj, op=None):
     return {
         'label': type_,
         'data': resolve_value(obj, {})
     }
 
 
-def IN_LIST(*args, context):
+def IN_LIST(*args, context, op=None):
     variants = []
     new_context = []
     for arg in args:
@@ -24,7 +24,7 @@ def IN_LIST(*args, context):
     return context
 
 
-def PATTERN(*args, context=None):
+def PATTERN(*args, context=None, op=None):
     new_ctx = []
     for arg in args:
         resolve_value(arg, new_ctx)
@@ -37,3 +37,11 @@ def WORD(*args, context, op=None):
         return context
     elif len(args) == 0:
         return context.append(('regex', '\\w+', op))
+
+def NUM(*args, context, op=None):
+    if len(args) == 1:
+        literal = args[0]
+        context.append(('value', literal, op))
+        return context
+    elif len(args) == 0:
+        return context.append(('regex', '\\d+', op))

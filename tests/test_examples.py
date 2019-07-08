96e9a6225c2d099545db5c739b16f6a0f1ea35b0
Sarunas Navickas <sarunas.navickas@tokenmill.lt>
2019-07-08 13:33:26 +0300

    Full example

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -1,8 +1,33 @@
+import spacy
+
 import rita
+
+from spacy.pipeline import EntityRuler
 
 
 def test_color_car():
-    result = rita.compile('examples/color-car.rita')
-    print(result)
-    assert len(result) == 2
-    assert False
+    patterns = rita.compile('examples/color-car.rita')
+    print(patterns)
+    assert len(patterns) == 2
+
+    # Build Spacy
+    nlp = spacy.load('en')
+    ruler = EntityRuler(nlp, overwrite_ents=True)
+    ruler.add_patterns(patterns)
+
+    nlp.add_pipe(ruler)
+
+    # Load example
+
+    text = '''
+    Johny Silver was driving a red car. It was BMW X6.
+    '''
+
+    doc = nlp(text)
+
+    entities = [(e.text, e.label_) for e in doc.ents]
+    print(entities)
+
+    assert entities[0] == ('Johny Silver', 'PERSON',) # Normal NER
+    assert entities[1] == ('red car', 'CAR_COLOR') # Our first rule
+    assert entities[2] == ('BMW X6', 'CAR_MODEL') # Our second rule

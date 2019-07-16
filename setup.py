abdf0556c3a6d79168efe28441e3d48cfab8cbae
Šarūnas Navickas <zaibacu@gmail.com>
2019-07-16 20:43:13 +0300

    Small version bump

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="rita-dsl",
-    version="0.1.0",
+    version="0.1.1",
     long_description=desc,
     long_description_content_type="text/markdown",
     description="DSL for building language rules",

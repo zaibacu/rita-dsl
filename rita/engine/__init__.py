51201d40286d8db245ac8c6ae47214bb848f7d07
Šarūnas Navickas <zaibacu@gmail.com>
2019-11-16 17:59:43 +0200

    Fix engine selector

--- rita/engine/__init__.py
+++ rita/engine/__init__.py
@@ -1,8 +1,9 @@
+try:
+    import spacy
+    from .translate_spacy import compile_tree
+except ImportError:
+    from .translate_standalone import compile_tree
+
+
 def get_default():
-    try:
-        import spacy
-        from .translate_spacy import compile_tree
-        return compile_tree
-    except ImportError:
-        from .translate_standalone import compile_tree
-        return compile_tree
+    return compile_tree

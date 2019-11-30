424a5768ed4fa08f76509a5f8572639667792678
Šarūnas Navickas <zaibacu@gmail.com>
2019-11-30 17:17:03 +0200

    Generalize spaCy tests

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -2,30 +2,29 @@ import pytest
 import rita
 
 
-def test_color_car():
+@pytest.fixture
+def spacy_engine(request):
     spacy = pytest.importorskip("spacy", minversion="2.1")
+    def parser(rules_path):
+        patterns = rita.compile(rules_path)
+        nlp = spacy.load("en")
+        ruler = spacy.pipeline.EntityRuler(nlp, overwrite_ents=True)
+        ruler.add_patterns(patterns)
+        nlp.add_pipe(ruler)
+        def parse(text):
+            doc = nlp(text)
+            return list([(e.text, e.label_) for e in doc.ents])
+        return parse
 
-    patterns = rita.compile("examples/color-car.rita")
-    print(patterns)
-    assert len(patterns) == 3
+    return parser
 
-    # Build Spacy
-    nlp = spacy.load("en")
-    ruler = spacy.pipeline.EntityRuler(nlp, overwrite_ents=True)
-    ruler.add_patterns(patterns)
 
-    nlp.add_pipe(ruler)
-
-    # Load example
-
+def test_color_car(spacy_engine):
     text = """
     John Silver was driving a red car. It was BMW X6 Mclass. John likes driving it very much.
     """
-
-    doc = nlp(text)
-
-    entities = [(e.text, e.label_) for e in doc.ents]
-    print(entities)
+    parser = spacy_engine("examples/color-car.rita")
+    entities =  parser(text)
 
     assert entities[0] == ("John Silver", "PERSON")  # Normal NER
     assert entities[1] == ("red car", "CAR_COLOR")  # Our first rule
@@ -33,42 +32,25 @@ def test_color_car():
     assert entities[3] == ("John likes driving", "LIKED_ACTION")  # Our third rule
 
 
-def test_fuzzy_matching():
-    spacy = pytest.importorskip("spacy", minversion="2.1")
-
-    patterns = rita.compile("examples/fuzzy-matching.rita")
-    print(patterns)
-    assert len(patterns) == 1
-
-    # Build Spacy
-    nlp = spacy.load("en")
-    ruler = spacy.pipeline.EntityRuler(nlp, overwrite_ents=True)
-    ruler.add_patterns(patterns)
-
-    nlp.add_pipe(ruler)
+def test_fuzzy_matching(spacy_engine):
+    parser = spacy_engine("examples/fuzzy-matching.rita")
 
     # Check if we're matching with capitalized word
-
     t1 = """
     Squirrel just walked outside
     """
 
-    doc = nlp(t1)
-
-    entities = [(e.text, e.label_) for e in doc.ents]
+    entities = parser(t1)
 
     assert len(entities) == 1
     assert entities[0] == ("Squirrel", "CRITTER")
 
     # Check if we're matching with all CAPS
-
     t2 = """
     there's a SQUIRREL
     """
 
-    doc = nlp(t2)
-
-    entities = [(e.text, e.label_) for e in doc.ents]
+    entities = parser(t2)
 
     assert len(entities) == 1
     assert entities[0] == ("SQUIRREL", "CRITTER")
@@ -83,5 +65,3 @@ def test_standalone_simple():
 
     assert entities[0] == ("Donald Trump was elected", "WON_ELECTION")
     assert entities[1] == ("defeating Hilary Clinton", "LOST_ELECTION")
-
-    

8c701aed91e1407c5591f4320ec9321b27eb31ea
Šarūnas Navickas <zaibacu@gmail.com>
2019-12-07 21:04:37 +0200

    Cover cases on standalone

--- tests/test_rules.py
+++ tests/test_rules.py
@@ -66,6 +66,10 @@ class TestSpacy(object):
         
 
 class TestStandalone(object):
+    @property
+    def punct(self):
+        return re.compile(r"[.,!;?:]")
+
     def compiler(self, rules):
         from rita.engine.translate_standalone import compile_rules
         return rita.compile_string(rules, compile_fn=compile_rules).patterns
@@ -84,4 +88,38 @@ class TestStandalone(object):
         print(rules)
         assert len(rules) == 1
         assert rules[0] == re.compile(r"(?P<MULTI_LABEL>(test1|test2))", re.IGNORECASE)
+
+    def test_or_branch(self):
+        rules = self.compiler('''
+        {WORD("test1")|WORD("test2")}->MARK("SPLIT_LABEL")
+        ''')
+        print(rules)
+        assert len(rules) == 2
+        assert rules[0] == re.compile(r"(?P<SPLIT_LABEL>(test1))", re.IGNORECASE)
+        assert rules[1] == re.compile(r"(?P<SPLIT_LABEL>(test2))", re.IGNORECASE)
+
+    def test_or_branch_multi(self):
+        rules = self.compiler('''
+        {WORD("test1")|WORD("test2"),WORD("test3")|WORD("test4")}->MARK("MULTI_SPLIT_LABEL")
+        ''')
+        print(rules)
+        assert len(rules) == 4
+        assert rules[0] == re.compile(r"(?P<MULTI_SPLIT_LABEL>(test1)[.,!;?:]?\s(test3))", re.IGNORECASE)
+        assert rules[1] == re.compile(r"(?P<MULTI_SPLIT_LABEL>(test2)[.,!;?:]?\s(test3))", re.IGNORECASE)
+        assert rules[2] == re.compile(r"(?P<MULTI_SPLIT_LABEL>(test1)[.,!;?:]?\s(test4))", re.IGNORECASE)
+        assert rules[3] == re.compile(r"(?P<MULTI_SPLIT_LABEL>(test2)[.,!;?:]?\s(test4))", re.IGNORECASE)
+        
+
+
+    def test_or_branch_multi_w_single(self):
+        rules = self.compiler('''
+        numbers={"one", "two", "three"}
+        {WORD("test1")|WORD("test2"),IN_LIST(numbers),WORD("test3")|WORD("test4")}->MARK("MULTI_SPLIT_LABEL")
+        ''')
+        print(rules)
+        assert len(rules) == 4
+        assert rules[0] == re.compile(r"(?P<MULTI_SPLIT_LABEL>(test1)[.,!;?:]?\s(one|two|three)[.,!;?:]?\s(test3))", re.IGNORECASE)
+        assert rules[1] == re.compile(r"(?P<MULTI_SPLIT_LABEL>(test2)[.,!;?:]?\s(one|two|three)[.,!;?:]?\s(test3))", re.IGNORECASE)
+        assert rules[2] == re.compile(r"(?P<MULTI_SPLIT_LABEL>(test1)[.,!;?:]?\s(one|two|three)[.,!;?:]?\s(test4))", re.IGNORECASE)
+        assert rules[3] == re.compile(r"(?P<MULTI_SPLIT_LABEL>(test2)[.,!;?:]?\s(one|two|three)[.,!;?:]?\s(test4))", re.IGNORECASE)
 

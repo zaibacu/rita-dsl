db1cee186d5297a355fa739f9effb31678296d8e
Šarūnas Navickas <sarunas.navickas@tokenmill.lt>
2019-12-17 20:57:45 +0200

    More style changes

--- tests/test_rules.py
+++ tests/test_rules.py
@@ -3,23 +3,24 @@ import re
 import pytest
 import rita
 
-from utils import spacy_engine, standalone_engine
-
 
 class TestSpacy(object):
     @property
     def punct(self):
         return {'IS_PUNCT': True, 'OP': '?'}
-    
+
     def compiler(self, rules):
-        spacy = pytest.importorskip("spacy", minversion="2.1")
+        pytest.importorskip("spacy", minversion="2.1")
         return rita.compile_string(rules, use_engine="spacy")
-        
+
     def test_single_word(self):
         rules = self.compiler('WORD("Test")->MARK("SOME_LABEL")')
         print(rules)
         assert len(rules) == 1
-        assert rules[0] == {"pattern": [{"LOWER": "test"}], "label": "SOME_LABEL"}
+        assert rules[0] == {
+            "pattern": [{"LOWER": "test"}],
+            "label": "SOME_LABEL"
+        }
 
     def test_multiple_words(self):
         rules = self.compiler('''
@@ -28,7 +29,10 @@ class TestSpacy(object):
         ''')
         print(rules)
         assert len(rules) == 1
-        assert rules[0] == {"pattern": [{"LOWER": {"REGEX": "(test1|test2)"}}], "label": "MULTI_LABEL"}
+        assert rules[0] == {
+            "pattern": [{"LOWER": {"REGEX": "(test1|test2)"}}],
+            "label": "MULTI_LABEL"
+        }
 
     def test_simple_pattern(self):
         rules = self.compiler('''
@@ -36,8 +40,10 @@ class TestSpacy(object):
         ''')
         print(rules)
         assert len(rules) == 1
-        assert rules[0] == {"pattern": [{"LOWER": "test1"}, self.punct, {"LOWER": "test2"}], "label": "SIMPLE_PATTERN"}
-        
+        assert rules[0] == {
+            "pattern": [{"LOWER": "test1"}, self.punct, {"LOWER": "test2"}],
+            "label": "SIMPLE_PATTERN"
+        }
 
     def test_or_branch(self):
         rules = self.compiler('''
@@ -45,8 +51,14 @@ class TestSpacy(object):
         ''')
         print(rules)
         assert len(rules) == 2
-        assert rules[0] == {"pattern": [{"LOWER": "test1"}], "label": "SPLIT_LABEL"}
-        assert rules[1] == {"pattern": [{"LOWER": "test2"}], "label": "SPLIT_LABEL"}
+        assert rules[0] == {
+            "pattern": [{"LOWER": "test1"}],
+            "label": "SPLIT_LABEL"
+        }
+        assert rules[1] == {
+            "pattern": [{"LOWER": "test2"}],
+            "label": "SPLIT_LABEL"
+        }
 
     def test_or_branch_multi(self):
         rules = self.compiler('''
@@ -54,10 +66,22 @@ class TestSpacy(object):
         ''')
         print(rules)
         assert len(rules) == 4
-        assert rules[0] == {"pattern": [{"LOWER": "test1"}, self.punct, {"LOWER": "test3"}], "label": "MULTI_SPLIT_LABEL"}
-        assert rules[1] == {"pattern": [{"LOWER": "test2"}, self.punct, {"LOWER": "test3"}], "label": "MULTI_SPLIT_LABEL"}
-        assert rules[2] == {"pattern": [{"LOWER": "test1"}, self.punct, {"LOWER": "test4"}], "label": "MULTI_SPLIT_LABEL"}
-        assert rules[3] == {"pattern": [{"LOWER": "test2"}, self.punct, {"LOWER": "test4"}], "label": "MULTI_SPLIT_LABEL"}
+        assert rules[0] == {
+            "pattern": [{"LOWER": "test1"}, self.punct, {"LOWER": "test3"}],
+            "label": "MULTI_SPLIT_LABEL"
+        }
+        assert rules[1] == {
+            "pattern": [{"LOWER": "test2"}, self.punct, {"LOWER": "test3"}],
+            "label": "MULTI_SPLIT_LABEL"
+        }
+        assert rules[2] == {
+            "pattern": [{"LOWER": "test1"}, self.punct, {"LOWER": "test4"}],
+            "label": "MULTI_SPLIT_LABEL"
+        }
+        assert rules[3] == {
+            "pattern": [{"LOWER": "test2"}, self.punct, {"LOWER": "test4"}],
+            "label": "MULTI_SPLIT_LABEL"
+        }
 
     def test_or_branch_multi_w_single(self):
         rules = self.compiler('''
@@ -67,10 +91,20 @@ class TestSpacy(object):
         print(rules)
         assert len(rules) == 4
         list_items = {"LOWER": {"REGEX": "(one|three|two)"}}
-        assert rules[0] == {"pattern": [{"LOWER": "test1"}, self.punct, list_items, self.punct, {"LOWER": "test3"}], "label": "MULTI_SPLIT_LABEL"}
-        assert rules[1] == {"pattern": [{"LOWER": "test2"}, self.punct, list_items, self.punct, {"LOWER": "test3"}], "label": "MULTI_SPLIT_LABEL"}
-        assert rules[2] == {"pattern": [{"LOWER": "test1"}, self.punct, list_items, self.punct, {"LOWER": "test4"}], "label": "MULTI_SPLIT_LABEL"}
-        assert rules[3] == {"pattern": [{"LOWER": "test2"}, self.punct, list_items, self.punct, {"LOWER": "test4"}], "label": "MULTI_SPLIT_LABEL"}
+        assert rules[0] == {
+            "pattern": [{"LOWER": "test1"}, self.punct, list_items, self.punct, {"LOWER": "test3"}],
+            "label": "MULTI_SPLIT_LABEL"
+        }
+        assert rules[1] == {
+            "pattern": [{"LOWER": "test2"}, self.punct, list_items, self.punct, {"LOWER": "test3"}], "label": "MULTI_SPLIT_LABEL"}
+        assert rules[2] == {
+            "pattern": [{"LOWER": "test1"}, self.punct, list_items, self.punct, {"LOWER": "test4"}],
+            "label": "MULTI_SPLIT_LABEL"
+        }
+        assert rules[3] == {
+            "pattern": [{"LOWER": "test2"}, self.punct, list_items, self.punct, {"LOWER": "test4"}],
+            "label": "MULTI_SPLIT_LABEL"
+        }
 
     def test_branching_list(self):
         rules = self.compiler('''
@@ -79,8 +113,14 @@ class TestSpacy(object):
         ''')
         print(rules)
         assert len(rules) == 2
-        assert rules[0] == {"label": "SPLIT_LIST", "pattern": [{"LOWER": {"REGEX": "(test1|test2|test4)"}}]}
-        assert rules[1] == {"label": "SPLIT_LIST", "pattern": [{"LOWER": "test"}, {"LOWER": "-"}, {"LOWER": "3"}]}
+        assert rules[0] == {
+            "label": "SPLIT_LIST",
+            "pattern": [{"LOWER": {"REGEX": "(test1|test2|test4)"}}]
+        }
+        assert rules[1] == {
+            "label": "SPLIT_LIST",
+            "pattern": [{"LOWER": "test"}, {"LOWER": "-"}, {"LOWER": "3"}]
+        }
 
     def test_double_branching_list(self):
         rules = self.compiler('''
@@ -89,10 +129,19 @@ class TestSpacy(object):
         ''')
         print(rules)
         assert len(rules) == 3
-        assert rules[0] == {"label": "SPLIT_LIST", "pattern": [{"LOWER": {"REGEX": "(test1|test2|test4)"}}]}
-        assert rules[1] == {"label": "SPLIT_LIST", "pattern": [{"LOWER": "test"}, {"LOWER": "-"}, {"LOWER": "3"}]}
-        assert rules[2] == {"label": "SPLIT_LIST", "pattern": [{"LOWER": "test"}, {"LOWER": "-"}, {"LOWER": "5"}]}
-        
+        assert rules[0] == {
+            "label": "SPLIT_LIST",
+            "pattern": [{"LOWER": {"REGEX": "(test1|test2|test4)"}}]
+        }
+        assert rules[1] == {
+            "label": "SPLIT_LIST",
+            "pattern": [{"LOWER": "test"}, {"LOWER": "-"}, {"LOWER": "3"}]
+        }
+        assert rules[2] == {
+            "label": "SPLIT_LIST",
+            "pattern": [{"LOWER": "test"}, {"LOWER": "-"}, {"LOWER": "5"}]
+        }
+
 
 class TestStandalone(object):
     @property
@@ -105,7 +154,7 @@ class TestStandalone(object):
 
     def compiler(self, rules):
         return rita.compile_string(rules, use_engine="standalone").patterns
-    
+
     def test_single_word(self):
         rules = self.compiler('WORD("Test")->MARK("SOME_LABEL")')
         print(rules)
@@ -156,10 +205,22 @@ class TestStandalone(object):
         ''')
         print(rules)
         assert len(rules) == 4
-        assert rules[0] == re.compile(r"(?P<MULTI_SPLIT_LABEL>(test1)[.,!;?:]?\s(one|three|two)[.,!;?:]?\s(test3))", self.flags)
-        assert rules[1] == re.compile(r"(?P<MULTI_SPLIT_LABEL>(test2)[.,!;?:]?\s(one|three|two)[.,!;?:]?\s(test3))", self.flags)
-        assert rules[2] == re.compile(r"(?P<MULTI_SPLIT_LABEL>(test1)[.,!;?:]?\s(one|three|two)[.,!;?:]?\s(test4))", self.flags)
-        assert rules[3] == re.compile(r"(?P<MULTI_SPLIT_LABEL>(test2)[.,!;?:]?\s(one|three|two)[.,!;?:]?\s(test4))", self.flags)
+        assert rules[0] == re.compile(
+            r"(?P<MULTI_SPLIT_LABEL>(test1)[.,!;?:]?\s(one|three|two)[.,!;?:]?\s(test3))",
+            self.flags
+        )
+        assert rules[1] == re.compile(
+            r"(?P<MULTI_SPLIT_LABEL>(test2)[.,!;?:]?\s(one|three|two)[.,!;?:]?\s(test3))",
+            self.flags
+        )
+        assert rules[2] == re.compile(
+            r"(?P<MULTI_SPLIT_LABEL>(test1)[.,!;?:]?\s(one|three|two)[.,!;?:]?\s(test4))",
+            self.flags
+        )
+        assert rules[3] == re.compile(
+            r"(?P<MULTI_SPLIT_LABEL>(test2)[.,!;?:]?\s(one|three|two)[.,!;?:]?\s(test4))",
+            self.flags
+        )
 
     def test_branching_list(self):
         rules = self.compiler('''
@@ -170,4 +231,3 @@ class TestStandalone(object):
         assert len(rules) == 2
         assert rules[0] == re.compile(r"(?P<SPLIT_LIST>(test1|test2|test4))", self.flags)
         assert rules[1] == re.compile(r"(?P<SPLIT_LIST>(test-3))", self.flags)
-

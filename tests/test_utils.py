88386684543d2cc98e84ed939c56e19f7561eda1
Šarūnas Navickas <zaibacu@gmail.com>
2020-01-25 15:24:37 +0200

    Cleanup

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -6,16 +6,3 @@ class TestDeaccent(object):
         assert deaccent("Šarūnas") == "Sarunas"
         assert deaccent("Kęstutis") == "Kestutis"
         assert deaccent("Ąžuolas") == "Azuolas"
-
-
-def test():
-    rules = """
-    cuts = {"fitted", "wide-cut"}
-    lengths = {"short", "long", "calf-length", "knee-length"}
-    fabric_types = {"soft", "airy", "crinkled"}
-    fabrics = {"velour", "chiffon", "knit", "woven", "stretch"}
-
-    {IN_LIST(cuts)?, IN_LIST(lengths), WORD("dress")}->MARK("DRESS_TYPE")
-    {IN_LIST(lengths), IN_LIST(cuts), WORD("dress")}->MARK("DRESS_TYPE")
-    {IN_LIST(fabric_types)?, IN_LIST(fabrics)}->MARK("DRESS_FABRIC")
-    """

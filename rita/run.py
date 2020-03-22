0548aa59ad8187cc90e4bd4d6b736d4e7e8a4603
Sarunas Navickas <zaibacu@gmail.com>
2020-03-22 19:27:02 +0200

    Allow to select engine when compiling

--- rita/run.py
+++ rita/run.py
@@ -1,7 +1,11 @@
 import json
 import argparse
+import logging
 
 import rita
+
+
+logger = logging.getLogger(__name__)
 
 
 def main():
@@ -14,9 +18,19 @@ def main():
         "out",
         help="output .jsonl file to store rules"
     )
+    parser.add_argument("--debug", help="debug mode", action="store_true")
+    parser.add_argument("--engine", help="Engine to use when compiling rules", default="spacy")
     args = parser.parse_args()
 
-    patterns = rita.compile(args.f)
+    if args.debug:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.INFO)
+
+
+    patterns = rita.compile(args.f, use_engine=args.engine)
+
+    logger.info("Compiling rules using {} engine".format(args.engine))
 
     with open(args.out, "w") as f:
         for pattern in patterns:

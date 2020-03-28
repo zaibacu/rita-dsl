0bc1c63e936b1c41e9073f22e94259baeeb55e1a
Šarūnas Navickas <zaibacu@gmail.com>
2020-03-28 21:43:25 +0200

    Handle cyclical case

--- tests/test_precompile.py
+++ tests/test_precompile.py
@@ -1,3 +1,5 @@
+import pytest
+
 from rita.precompile import precompile
 
 
@@ -9,3 +11,12 @@ def test_rule_import():
     result = precompile(rules.strip())
     with open("examples/simple-match.rita", "r") as f:
         assert result == f.read()
+
+
+def test_cyclical_import():
+    rules = """
+    @import "examples/cyclical-import.rita"
+    """
+
+    with pytest.raises(RuntimeError):
+        precompile(rules)

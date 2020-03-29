7aad1b71b6f26cff3e9ed7e8d945d8de5137d7c3
Šarūnas Navickas <zaibacu@gmail.com>
2020-03-29 14:24:05 +0300

    Remove unused classifiers

--- setup.py
+++ setup.py
@@ -35,9 +35,6 @@ setup(
     setup_requires=["pytest-runner"],
     entry_points={"console_scripts": ["rita = rita.run:main"]},
     classifiers=[
-        # Trove classifiers
-        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
-        "License :: OSI Approved :: MIT License",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.5",

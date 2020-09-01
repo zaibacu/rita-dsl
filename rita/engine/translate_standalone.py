8f4d7ff772bbb70942103a8f9f0a427038530576
Šarūnas Navickas <zaibacu@gmail.com>
2020-09-01 17:21:45 +0300

    Quick fix

--- rita/engine/translate_standalone.py
+++ rita/engine/translate_standalone.py
@@ -22,7 +22,7 @@ def apply_operator(syntax, op):
 
 def any_of_parse(lst, config, op=None):
     clause = r"(^|\s)(({0})\s?)".format("|".join(sorted(lst, key=lambda x: (-len(x), x))))
-    return "(" + apply_operator(clause, op) + ")"
+    return apply_operator(clause, op)
 
 
 def regex_parse(r, config, op=None):

e0a80912e635799b2c509dd0369360267f553d85
Roland Mueller <roland.m.mueller@googlemail.com>
2020-10-02 10:52:10 +0200

    Add TAG_WORD macro to Tag module

--- rita/modules/tag.py
+++ rita/modules/tag.py
@@ -1,9 +1,27 @@
 from rita.macros import resolve_value
 
 
-def TAG(name, config, op=None):
+def TAG(tag, config, op=None):
     """
     For generating POS/TAG patterns based on a Regex
     e.g. TAG("^NN|^JJ") for nouns or adjectives
     """
-    return "tag", resolve_value(name, config=config), op
+    values = {"tag": tag}
+    return "tag", values, op
+
+
+def TAG_WORD(tag, value, config, op=None):
+    """
+    For generating TAG patterns with a word or a list
+    e.g. match only "proposed" when it is in the sentence a verb (and not an adjective):
+    TAG_WORD("^VB", "proposed")
+    e.g. match a list of words only to verbs
+    words = {"percived", "proposed"}
+    {TAG_WORD("^VB", words)?}->MARK("LABEL")
+    """
+    values = {"tag": tag}
+    if type(value) == list:
+        values["list"] = value
+    else:
+        values["word"] = value
+    return "tag", values, op

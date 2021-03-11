4b5eb2991db180091151c0f8d90e6a7664b67b1b
Šarūnas Navickas <zaibacu@gmail.com>
2021-03-11 19:02:21 +0200

    Bump version

--- rita/__init__.py
+++ rita/__init__.py
@@ -11,7 +11,7 @@ from rita.utils import timer, Timer
 
 logger = logging.getLogger(__name__)
 
-__version__ = (0, 7, 0, os.getenv("VERSION_PATCH"))
+__version__ = (0, 7, 1, os.getenv("VERSION_PATCH"))
 
 
 def get_version():
